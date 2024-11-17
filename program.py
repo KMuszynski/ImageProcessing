ed25d32fde2276d4f621ff6ebc409e071b4400b1
Krzysztof Muszyński <krzysztofmuszynski@MacBook-Air-Krzysztof.local>
2024-11-17 20:40:11 +0100

    fixed bug

--- program.py
+++ program.py
@@ -6,7 +6,7 @@ from components.functions.elementary import doBrightness, doContrast, doNegative
 from components.functions.geometric import doHorizontalFlip, doVerticalFlip, doDiagonalFlip, doShrink, doEnlarge
 from components.functions.noise import doMedianFilter, doGeometricMeanFilter
 from components.functions.histogram import calculate_histogram
-from components.functions.filtration import universal_filter, optimized_slowpass_filterfrom
+from components.functions.filtration import universal_filter, optimized_slowpass_filter
 from components.functions.rayleigh import apply_rayleigh_pdf
 
 
@@ -42,7 +42,7 @@ def print_help():
     print(help_text)
 
 
-noParamFunctions = ["--negative", "--help", "--hflip", "--vflip", "--dflip"]
+noParamFunctions = ["--negative", "--help", "--hflip", "--vflip", "--dflip",'--slowpass']
 
 # Check if no command line parameters were given
 if len(sys.argv) == 1:
@@ -100,6 +100,10 @@ elif command == '--median':
     result_arr = doMedianFilter(param, arr)
 elif command == '--gmean':
     result_arr = doGeometricMeanFilter(param, arr)
+elif command == '--slowpass':
+    result_arr = optimized_slowpass_filter(arr)
+elif command == '--universal':
+    result_arr = universal_filter(arr, int(param))
 elif command == '--histogram':
     if param not in ["gray", "red", "green", "blue"]:
         print("Invalid channel! Choose from 'gray', 'red', 'green', or 'blue'.")
